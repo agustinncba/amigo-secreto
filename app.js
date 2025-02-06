274ca43a69a1a86da7569b40d191264d0e395d08
Agustin Rios <agustinncba@gmail.com>
2025-02-06 01:49:56 -0300

    Se agregaron los archivos del proyecto

---
+++ app.js
@@ -0,0 +1,14 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let amigo = document.getElementById("amigo").value;
+
+  if (amigo != "") {
+    amigos.push(amigo);
+     document.getElementById('amigo').value = "";
+  } else {
+    alert("Porfavor ingrese un amigo");
+  }
+  console.log(amigos);
+}
